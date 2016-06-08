51f31feec5b040f8af9c01764be8d49b8141ff76
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-08 14:11:28 -0400

    Update 1.1 Is Unique.java

--- Chapter-1-Arrays-and-Strings/1.1 Is Unique.java
+++ Chapter-1-Arrays-and-Strings/1.1 Is Unique.java
@@ -2,12 +2,12 @@ boolean isUnique(String s){
         if(s == null || s.length() < 0)
                 return true;
 
-        boolean[] used = new boolean[26];
+        boolean[] used = new boolean[128];
         for(int i = 0; i < s.length(); i++)
-                if(used[s.charAt(i) - 'a'])
+                if(used[s.charAt(i)])
                         return false;
                 else
-                        used[s.charAt(i) - 'a'] = true;
+                        used[s.charAt(i)] = true;
 
         return true;
 }
