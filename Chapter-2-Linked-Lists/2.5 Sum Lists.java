e15e94204b39448175ba85b8512a4b3817b76707
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-10 15:05:48 -0400

    Update 2.5 Sum Lists.java

--- Chapter-2-Linked-Lists/2.5 Sum Lists.java
+++ Chapter-2-Linked-Lists/2.5 Sum Lists.java
@@ -36,3 +36,59 @@ static LinkedListNode sumLists(LinkedListNode a, LinkedListNode b){
         
         return res.next;
     }
+    
+    
+    //  forward order;
+    //  #1: using stack;
+    //  #2: reverse each linked list;
+
+    static LinkedListNode sumLists(LinkedListNode a, LinkedListNode b){
+        LinkedListNode currA = a, currB = b;
+        LinkedListNode res = new LinkedListNode(0), curr = res;
+
+        Stack<Integer> stackA = new Stack<>();
+        Stack<Integer> stackB = new Stack<>();
+
+        //  Fill the stack;
+        while(currA != null){
+            stackA.push(currA.val);
+            currA = currA.next;
+        }
+
+        while(currB != null){
+            stackB.push(currB.val);
+            currB = currB.next;
+        }
+        
+        //  Construct res linked list;
+        int carry = 0;
+        while(!stackA.isEmpty() && !stackB.isEmpty()){
+            int ta = stackA.pop(), tb = stackB.pop();
+            curr.next = new LinkedListNode((ta + tb + carry) % 10);
+            carry = (ta + tb + carry) / 10;
+            
+            curr = curr.next;
+        }
+        
+        if(stackA.isEmpty())
+            while(!stackB.isEmpty()){
+                int tb = stackB.pop();
+                curr.next = new LinkedListNode((tb + carry) % 10);
+                carry = (tb + carry) / 10;
+                
+                curr = curr.next;
+            }
+        else
+            while(!stackA.isEmpty()){
+                int ta = stackA.pop();
+                curr.next = new LinkedListNode((ta + carry) % 10);
+                carry = (ta + carry) / 10;
+
+                curr = curr.next;
+            }
+        
+        if(carry != 0)
+            curr.next = new LinkedListNode(carry);
+
+        return res.next;
+    }
