939518ef9a30e35ebba1528d8ddc5f47c613214d
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-12 15:17:58 -0400

    Create 4.5 Validate BST.java

---
+++ Chapter-4-Trees-and-Graphs/4.5 Validate BST.java
@@ -0,0 +1,7 @@
+#1
+//  Inorder traversal gets an array;
+//  If BST, then the array should be sorted; vice versa.
+
+#2
+check every node
+and every node return its min_Value and max_Value of its subtrees;
