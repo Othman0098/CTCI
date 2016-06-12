2acaa0f08077dca856794ea67f9d1486626ab9a7
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-12 15:09:40 -0400

    Create 4.4 Check Balanced.java

---
+++ Chapter-4-Trees-and-Graphs/4.4 Check Balanced.java
@@ -0,0 +1,5 @@
+//  Comparing each node`s two subtree height, if any pair differ more than one, the tree is not balanced; else balanced;
+
+private int getHeight(TreeNode root){
+    //  Return the Height of the tree(subtree) begin at root;
+}
