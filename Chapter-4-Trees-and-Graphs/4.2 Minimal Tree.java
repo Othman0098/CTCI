6bc97c1cbebba121e281d3fe473073a0db0bf279
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-12 14:54:48 -0400

    Create 4.2 Minimal Tree.java

---
+++ Chapter-4-Trees-and-Graphs/4.2 Minimal Tree.java
@@ -0,0 +1,8 @@
+static TreeNode generateMinimalTree(int[] sortedArray){
+        //  Calculate the pivot to divide the array to 2 halves;
+        int pivot = sortedArray.length / 2;
+        
+        //  Recursively call generateMinimalTree() to generate minimal tree for each half;
+        ...
+        ...
+    }
