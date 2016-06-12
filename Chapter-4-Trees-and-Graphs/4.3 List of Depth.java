8fd0c4b8f462c477f2332dc180adba4d9ee41017
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-12 15:03:13 -0400

    Create 4.3 List of Depth.java

---
+++ Chapter-4-Trees-and-Graphs/4.3 List of Depth.java
@@ -0,0 +1,10 @@
+//  Level-Traversal of tree;
+
+List<List<Integer>> listOfDepths(TreeNode root){
+    Queue curr, next;
+    
+    //  Every time traverse all the nodes in curr;
+    //  put all children of those nodes in next;
+    //  After curr traversal finished, make curr = next, next = new queue();
+    //  Begin another lever traversal until curr.isEmpty();
+}
