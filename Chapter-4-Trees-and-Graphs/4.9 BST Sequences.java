835915fd65f9e16ed4e00b7905473e0d1263269e
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-12 16:58:35 -0400

    Create 4.9 BST Sequences.java

---
+++ Chapter-4-Trees-and-Graphs/4.9 BST Sequences.java
@@ -0,0 +1,8 @@
+//  level-traversal
+
+List<List<Integer>> getBSTSequences(TreeNode root){
+    //  level-traversal store in List<List<Integer>> inOrderTraversal;
+    
+    //  for each level, generate all possible permutation, store in result List;
+    
+}
