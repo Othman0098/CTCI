9f81e0d3d157758159271a6536d7ea7a427d7b25
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-15 15:43:23 -0400

    Update 6.1 The Heavy Pill.java

--- Chapter-6-Math-and-Logic-Puzzles/6.1 The Heavy Pill.java
+++ Chapter-6-Math-and-Logic-Puzzles/6.1 The Heavy Pill.java
@@ -1,7 +1,9 @@
 /**
- * Choose 1 pill from the first bootle
+ * Lable 20 bottles from 1 to 20;
+ * Choose 1 pill from bottle 1, 2 pills from bottle 2, 3 ...
  * 
+ * Put the pills together on the scale, weight x;
  * 
- * 
+ * The heavy bottle:  (x - 20) / 0.1;
  * 
  */
