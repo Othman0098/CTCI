90ab8f8b4da335a0a5bf03d4eff525c4b675995f
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-14 22:26:16 -0400

    Create 5.4 Next Number.java

---
+++ Chapter-5-Bit-Manipulation/5.4 Next Number.java
@@ -0,0 +1,19 @@
+//  011111  ->  101111
+//  101100  ->  110011  
+
+int nextNumber(int num){
+
+    //  find first 1 bit;
+    int oneIdx = 0;
+    int tmp = num;
+    while(tmp & 1 == 0){
+        tmp >>>= 1;
+        oneIdx++;
+    }
+    
+    //  int zeroIdx = oneIdx;
+    while(num & 1 == 1){
+        tmp >>>= 1;
+        zeroIdx++;
+    }
+}
