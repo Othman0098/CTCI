9de43c948ad790ef74e59ab695e2c173487aa627
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-15 16:07:04 -0400

    Create 6.6 Blue-Eyed Island.java

---
+++ Chapter-6-Math-and-Logic-Puzzles/6.6 Blue-Eyed Island.java
@@ -0,0 +1,8 @@
+/**
+ * If only 1 with blue-eye. Take 1 day;
+ * If 2 with blue-eye, 1st day no one leave, 2nd all leave. Take 2 days;
+ * If 3 with blue-eye, 1st day no one leave, 2nd no one leave, 3rd every one aware that not only those two blue-eyed guys in sight have blue eyes. Take 3 days; 
+ * ...
+ * ...
+ * If n with blue-eye. Take (n - 1) days.
+ */
