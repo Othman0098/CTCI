6f9929d8abef36ac7bfa572b247fa3fb0b4c511a
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-15 16:00:56 -0400

    Create 6.5 Jugs of Water.java

---
+++ Chapter-6-Math-and-Logic-Puzzles/6.5 Jugs of Water.java
@@ -0,0 +1,8 @@
+/**
+ * Step0:     Five:   0   Three:  0
+ * Step1:     Five:   5   Three:  0
+ * Step2:     Five:   2   Three:  3
+ * Step3:     Five:   0   Three:  2
+ * Step4:     Five:   5   Three:  2
+ * Step5:     Five:   4   Three:  3
+ */
