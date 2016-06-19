ada4ee9570ab7a975b7becf3929eb2c2648ee764
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-19 13:51:06 -0400

    Create 10.1 Sorted Merge.java

---
+++ Chapter-10-Sorting-and-Searching/10.1 Sorted Merge.java
@@ -0,0 +1,25 @@
+int[] mergeSorted(int[] a, int[] b) {
+    int la = a.length;
+    int lb = b.length;
+    
+    int idxa = 0;
+    int idxb = 0;
+    
+    while (a.left < la && b.left < lb) {
+        if (a[idxa] <= b[idxb])
+            idxa++;
+        else {
+            a.startFrom(idxa).shiftByOne;
+            a[idxa] = b[idxb];
+            idxa++;
+            idxb++;
+        }
+    }
+    
+    if (idxb == lb)
+        return a;
+    else {
+        connect b to a;
+        return a;
+    }
+}
