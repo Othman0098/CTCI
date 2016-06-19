bf4ab6dab50c3f4d74c946b02a800963ba52e3db
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-19 16:33:30 -0400

    Create 10.9 Sorted Matrix Search.java

---
+++ Chapter-10-Sorting-and-Searching/10.9 Sorted Matrix Search.java
@@ -0,0 +1,12 @@
+/**
+ * Solution 1:
+ * Same as #240.
+ *
+ * Start from the right-up corner of the matrix.
+ *  if x > mat[i][j]
+ *      i++;
+ *  if x < mat[i][j]
+ *      j--;
+ *  Until find x or not.
+ *
+ */
