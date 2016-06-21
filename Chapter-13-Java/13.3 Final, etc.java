fcbd3bb3b8b9537023623c0fe96d48b04d260a19
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-21 14:34:00 -0400

    Update 13.3 Final, etc.java

--- Chapter-13-Java/13.3 Final, etc.java
+++ Chapter-13-Java/13.3 Final, etc.java
@@ -1,1 +1,16 @@
-
+/**
+ * final, finally, finalize():
+ * 
+ *  final:
+ *      Used to control whether a variable, method, or class is "changeable."
+ * 
+ *  finally:
+ *      Used in a try/catch block to ensure that a segment of code is always executed.
+ * 
+ *  finalize():
+ *      Called by the garbage collector once it determinse that no more reference exists.
+ * 
+ *      protected void finalize() throws Throwable {
+ *          // Close open files, release resources, etc.
+ *      }
+ */
