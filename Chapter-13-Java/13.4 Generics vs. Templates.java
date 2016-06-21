cf39291fa6d241d61b398436de07d62de0a02e20
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-21 14:55:35 -0400

    Update 13.4 Generics vs. Templates.java

--- Chapter-13-Java/13.4 Generics vs. Templates.java
+++ Chapter-13-Java/13.4 Generics vs. Templates.java
@@ -1,4 +1,13 @@
 /**
  * Java Generics:
- *
+ *      Is rooted in an idea of "type erasure." 
+ *      This techinique eliminates the parameterized type when source code is translated to the JVM byte code.
+ *      This didn`t really change much about our capabilities, just made things a bit prettier.
+ * 
  */
+
+/**
+ * C++ Templates:
+ *      Templates are essentially a glorified macro set, with the compiler creating a new copy of the template code for each type.
+ * 
+ * /
