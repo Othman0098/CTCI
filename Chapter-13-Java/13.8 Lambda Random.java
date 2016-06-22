2ff35fadb61ef208aa79154adc26082e1ed9f840
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-22 12:35:07 -0400

    Update 13.8 Lambda Random.java

--- Chapter-13-Java/13.8 Lambda Random.java
+++ Chapter-13-Java/13.8 Lambda Random.java
@@ -1,3 +1,40 @@
 /**
- *
+ *  
  */
+
+//  lambda expressions;
+List<Integer> getRandomSubset(list<Integer> list) {
+  Random random = new Random();
+  List<Integer> subset = list.stream().filter(
+      k -> { return random.nextBoolean();   /* Flip coin. */
+  }).collect(Collectors.toList());
+  return subset;
+}
+
+
+//  Using predicate
+Random random = new Random;
+Predicate<object> flipCoin = o -> {
+  return random.nextBoolean();
+};
+
+List<Integer> getRandomSubset(List<Integer> list) {
+  List<Integer> subset = list.stream().filter(flipCoin).
+    collect(Collectors.toList());
+  return subset;
+}
+
+
+//  Without lambda expressions;
+List<Integer> getRandomSubset(list<Integer> list) {
+  Random random = new Random();
+  List<Integer> subset = new ArrayList<Integer>();
+  for (int item : list) {
+    /* Flip coin. */
+    if (random.nextBoolean()) {
+      subset.add(item);
+    }
+  }
+  return subset;
+}
+
