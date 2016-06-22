e5aec215ad3e4410451cdfc41af7ab4e2e9ce9db
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-22 11:17:27 -0400

    Create 13.5 TreeMap, HashMap, LinkedHashMap.java

---
+++ Chapter-13-Java/13.5 TreeMap, HashMap, LinkedHashMap.java
@@ -0,0 +1,11 @@
+/**
+ *  HashMap offers O(1) lookup and insertion;   ordering of the keys is essentially arbitrary;  
+ *      HashMap is implemented by an array pf linked list.
+ *
+ *  TreeMap offers O(logN) lookup and insertion;    Keys are in sorted order, it must implement the Comparable interface;
+ *      TreeMap is implemented by a Red-Black Tree.
+ *  
+ *  LinkedHashMap offers O(1) lookup and insertion; Keys are ordered by their insertion order;
+ *      LinkedHashMap is implemented by doubly-linked buckets.
+ *
+ */
