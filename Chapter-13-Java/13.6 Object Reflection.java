189b2926940fc8ebfc363cb3da82c108928a07c7
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-22 11:23:59 -0400

    Update 13.6 Object Reflection.java

--- Chapter-13-Java/13.6 Object Reflection.java
+++ Chapter-13-Java/13.6 Object Reflection.java
@@ -1,3 +1,11 @@
 /**
+ *  Object Reflection is a feature in Java that provides a way to get reflective information about Java classes and objects, 
+ *  and preform operations such as:
+ * 
+ *  1.  Getting information about the methods and fields present inside the class at runtime.
+ * 
+ *  2.  Creating a new instance of class.
+ * 
+ *  3.  Getting and setting the object fields directly by getting field reference, regardless of what the acceess modifier is.
  * 
  */
