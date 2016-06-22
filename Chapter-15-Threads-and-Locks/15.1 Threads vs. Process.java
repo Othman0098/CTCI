cb79b8221cabba5af2f7b95bdd3e115509fd44a4
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-22 17:06:33 -0400

    Update 15.1 Threads vs. Process.java

--- Chapter-15-Threads-and-Locks/15.1 Threads vs. Process.java
+++ Chapter-15-Threads-and-Locks/15.1 Threads vs. Process.java
@@ -1,3 +1,13 @@
 /**
- *
+ *  Process:
+ *  As an instance of a program in execution. A process is an independent entity to which system resources (e.g., CPU time  and memory)
+ *  are allocated.
+ *  Each process is executed in a separate address space, and one process cannot access the variables and data structures of another 
+ *  process.
+ *  If a process wishes to access another process` resources, inter-process communications have to be used.
+ * 
  */
+ 
+ /**
+  * 
+  */
