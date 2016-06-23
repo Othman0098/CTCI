3cba22d18ec61e36b2d89c3b26f090e56f590565
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-23 15:40:15 -0400

    Create 15.3 Dining Philosophers.java

---
+++ Chapter-15-Threads-and-Locks/15.3 Dining Philosophers.java
@@ -0,0 +1,36 @@
+/**
+ *  To prevent deadlocks, we can implement a strategy where a philosopher will put down his left chopstick if he is unable to obtain 
+ *  the right one.
+ *
+ */
+
+public class Chopstick {
+  /* Same as before */
+  
+  public boolean pickUp() {
+    return lock.tryLock();
+  }
+}
+
+public class Philosopher extneds Thread {
+  /* Same as before */
+  
+  public void eat() {
+    if (pickUp()) {
+      chew();
+      putDown();
+    }
+  }
+  
+  public boolean pickUp() {
+    /* Attempt to pick up */
+    if (!left.pickUp()) {
+      return false;
+    }
+    if (!right.pickUp()) {
+      left.putDown();
+      return false;
+    }
+    return true;
+  }
+}
