97cb3c1b4167b502697437c76df1326b3c77f2e4
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-23 14:51:35 -0400

    Update 15.2 Context Switch.java

--- Chapter-15-Threads-and-Locks/15.2 Context Switch.java
+++ Chapter-15-Threads-and-Locks/15.2 Context Switch.java
@@ -1,3 +1,6 @@
 /**
- *
+ *  Context Switch:
+ *  Is the time spent switching between two processes;
+ *    (i.e., bringing a waiting process into execution and sending an executing process into waiting/terminated state).
+ * 
  */
