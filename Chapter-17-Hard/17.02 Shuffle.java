da2ad33531424fbb018b47ce2543ec96c22b5cef
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-24 15:34:16 -0400

    Update 17.02 Shuffle.java

--- Chapter-17-Hard/17.02 Shuffle.java
+++ Chapter-17-Hard/17.02 Shuffle.java
@@ -1,6 +1,7 @@
 Card[] shuffleCards(Card[] cards, Random random) {
     Card[] res = new Card[cards.length];
     for (int i = 0; i < cards.length; i++) {
+        //  Check if used?
         res[random.nextInt(cards.length)] = cards[i];
     }
     
