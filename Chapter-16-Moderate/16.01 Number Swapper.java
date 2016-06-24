4a8a4d662dcd56c7c64dd6b28c5a84ed8bb29141
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-24 13:51:37 -0400

    Update 16.01 Number Swapper.java

--- Chapter-16-Moderate/16.01 Number Swapper.java
+++ Chapter-16-Moderate/16.01 Number Swapper.java
@@ -1,7 +1,20 @@
+//  Solution #1;
+
 void numberSwapper(int a, int b) {
     a += b;
     b = a - b;
     a -= b;
     
     return;
+}
+
+
+//  Solution #2;
+//  Bit manipulation;
+
+void numberSwapper(int a, int b) {
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    
 }
