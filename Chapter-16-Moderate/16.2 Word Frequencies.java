7b7e0f87c7fc568c5da751dee7eba27edf681730
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-24 13:58:58 -0400

    Create 16.2 Word Frequencies.java

---
+++ Chapter-16-Moderate/16.2 Word Frequencies.java
@@ -0,0 +1,27 @@
+HashMap<String, Integer> setupDictionary(String[] book) {
+    HashMap<String, Integer> table = new HashMap<>();
+    
+    for (String word : book) {
+        word = word.toLowerCase();
+        if (word.trim() != "") {
+            if (!table.containsKey(word)) {
+                table.put(word, 0);
+            }
+            table.put(word.table.get(word) + 1);
+        }
+    }
+    
+    return table;
+}
+
+int getFrequency(HashMap<String, Integer> table, String word) {
+    if (table == null || word == null)
+        return -1;
+    
+    word = word.toLowerCase();
+    if (table.containsKey(word) {
+        return table.get(word);
+    }
+    
+    return 0;
+}
