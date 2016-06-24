498f461100f4a671ed4a1b3dcb28ba015a5bbe88
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-06-24 15:33:20 -0400

    Update 17.02 Shuffle.java

--- Chapter-17-Hard/17.02 Shuffle.java
+++ Chapter-17-Hard/17.02 Shuffle.java
@@ -6,3 +6,38 @@ Card[] shuffleCards(Card[] cards, Random random) {
     
     return res;
 }
+
+
+//  Recursive;
+
+int rand(int lower, int higher) {
+    return lower + (int)(Math.random() * (higher - lower + 1));
+}
+
+int[] shuffleArrayRecursively(int[] cards, int i) {
+    if (i == 0)
+        return cards;
+        
+    shuffleArrayRecursively(cards, i - 1);  //Shuffle earlier part;
+    int k = rand(0, i);                     //Pick random index to swap with;
+    
+    /* Swap element k and i */
+    int tmp = cards[k];
+    cards[k] = cards[i];
+    cards[i] = tmp;
+    
+    /* Return shuffled array */
+    return cards;
+}
+
+
+//  Iterative;
+void shuffleArrayIteratively(int[] cards) {
+    for (int i = 0; i < cards.length; i++) {
+        int k = rand(0, i);
+        int tmp = cards[k];
+        cards[k] = cards[i];
+        cards[i] = tmp;
+    }
+    return;
+}
