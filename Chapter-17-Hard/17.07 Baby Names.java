7831b0fa20d5022e571e7e5067df79a2fe78c7e6
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 14:06:29 -0400

    Update 17.07 Baby Names.java

--- Chapter-17-Hard/17.07 Baby Names.java
+++ Chapter-17-Hard/17.07 Baby Names.java
@@ -21,3 +21,25 @@ Graph constructGraph(HashMap<String, Integer> names) {
     }
     return graph;
 }
+
+/* Connect synonymous spellings. */
+void connectEdges(Graph graph, String[][] synonyms) {
+    for (String[] entry : synonyms) {
+        String name1 = entry[0];
+        String name2 = entry[1];
+        graph.addEdge(name1, name2);
+    }
+}
+
+/* Do DFS of each component. If a node has been visited before, then its component has already been computed. */
+HashMap<String, Integer> getTrueFrequencies(Graph graph) {
+    HashMap<String, Integer> rootNames = new HashMap<String, Integer>();
+    for (GraphNode node : graph.getNodes()) {
+        if (!node.isVisited()) { // Already visited this component
+            int frequency = getComponentFrequency(node);
+            String name = node.getName();
+            rootNames.put(name, frequency);
+        }
+    }
+    return rootNames;
+}
