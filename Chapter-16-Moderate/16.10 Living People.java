53a76b7687692219a36e5a03c99abd0aa99e867f
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 15:49:27 -0400

    Update 16.10 Living People.java

--- Chapter-16-Moderate/16.10 Living People.java
+++ Chapter-16-Moderate/16.10 Living People.java
@@ -1,1 +1,22 @@
+/**
+ * Solution:
+ * 
+ */
 
+int mostPeopleAliveYear(LinkedList<Person> people) {
+    int[] years = new int[2018];
+    
+    for (Person p : people) {
+        years[p.birth]++;
+        years[p.death + 1]--;
+    }
+    
+    int count = 0;
+    int max = 0;
+    for (int i = 0; i < 2018; i++) {
+        count += years[i];
+        max = Math.max(max, count);
+    }
+    
+    return max;
+}
