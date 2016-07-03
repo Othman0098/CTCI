e326d5ac04b7d59201fbfae4f1ddf9c0f5b021a8
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 16:19:21 -0400

    Update 16.11 Diving Board.java

--- Chapter-16-Moderate/16.11 Diving Board.java
+++ Chapter-16-Moderate/16.11 Diving Board.java
@@ -1,1 +1,32 @@
+/**
+ * Recursive Solution:
+ * 
+ * Takes O(2^k) time, since there are two choices at each recursive call and we recurse to a depth of k;
+ * 
+ */
 
+
+/**
+ * Memoization Solution:
+ * If seen (total, plank count) pair before then stop this recursive path.
+ * Using a HashSet with a key of (total, plank count);
+ * 
+ */
+ 
+
+/**
+ * Optimal Solution:
+ * 
+ * Just go through all unique sets of K planks(sets, not orders);
+ * 
+ */
+ 
+HashSet<Integer> allLengths(int k, int shorter, int longer) {
+    HashSet<Integer> lengths = new HashSet<Integer>();
+    for (int nShorter = 0; nShorter <= k; nShorter++) {
+        int nLonger = k - nShorter;
+        int length = nShorter * shorter + nLonger * longer;
+        lengths.add(length);
+    }
+    return lengths;
+}
