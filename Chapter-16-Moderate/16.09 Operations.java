bc1ba7d00a6efc672b9737fa2bc36b64515b8e9b
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 15:18:01 -0400

    Update 16.09 Operations.java

--- Chapter-16-Moderate/16.09 Operations.java
+++ Chapter-16-Moderate/16.09 Operations.java
@@ -14,4 +14,15 @@ int subtract(int a, int b) {
  * Standard Solution;
  * 
  */
+ 
+/* Flip a positive sign to negative or negative sign to pos. */
+int negate(int a) {
+    int neg = 0;
+    int newSign = a < 0 ? 1 : -1;
+    while (a != 0) {
+        neg += newSign;
+        a += newSign;
+    }
+    return neg;
+}
 
