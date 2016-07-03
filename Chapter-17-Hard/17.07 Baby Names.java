28e5ba5b70fd78ee1bc8dfb0c5126b2eb9937af6
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 13:59:40 -0400

    Update 17.07 Baby Names.java

--- Chapter-17-Hard/17.07 Baby Names.java
+++ Chapter-17-Hard/17.07 Baby Names.java
@@ -10,3 +10,14 @@ HashMap<String, Integer> trulyMostPopular(HashMap<String, Integer> names, String
     HashMap<String, Integer> rootNames = getTrueFrequencies(graph);
     return rootNames;
 }
+
+/* Add all names to graph as nodes. */
+Graph constructGraph(HashMap<String, Integer> names) {
+    Graph graph = new Graph();
+    for (Entry<String, Integer> entry : names.entrySet()) {
+        String name = entry.getKey();
+        int frequency = entry.getValue();
+        graph.createNode(name, frequency);
+    }
+    return graph;
+}
