9439f9bb81266d180d6cb124fa6d4f3d52b1b51a
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 15:22:16 -0400

    Update 16.09 Operations.java

--- Chapter-16-Moderate/16.09 Operations.java
+++ Chapter-16-Moderate/16.09 Operations.java
@@ -26,3 +26,19 @@ int negate(int a) {
     return neg;
 }
 
+/* Optimal Flip. */
+int negate(int a) {
+    int neg = 0;
+    int newSign = a < 0 ? 1 : -1;
+    int delta = newSign;
+    while (a != 0) {
+        boolean differentSigns = (a + delta > 0) != (a > 0);
+        if (a + delta != 0 && differentSigns) { // If delta is too big, reset it
+            delta = newSign;
+        }
+        neg += delta;
+        a += delta;
+        delta += delta; // Double the delta
+    }
+    return reg;
+}
