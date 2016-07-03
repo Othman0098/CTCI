0c6b4204488b15d5e9b0cbdc75c4798594e77aeb
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 14:35:32 -0400

    Update 17.08 Circus Tower.java

--- Chapter-17-Hard/17.08 Circus Tower.java
+++ Chapter-17-Hard/17.08 Circus Tower.java
@@ -1,1 +1,15 @@
-
+/**
+ * Dynamic Programming;
+ * 
+ * 导弹打飞机;
+ * Longest Increasing Sequence;
+ * ...
+ * 
+ * 
+ * Solution:
+ * 
+ * Sorting based on Height;
+ * Then iterate through all person from shortest to tallest;
+ * Decide if one can stand on another depends on theirs weights;
+ * 
+ */
