e8b4a50d64f6f0ff544da890d9d7d11e4d4cd4b1
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-03 15:09:55 -0400

    Update 16.09 Operations.java

--- Chapter-16-Moderate/16.09 Operations.java
+++ Chapter-16-Moderate/16.09 Operations.java
@@ -1,1 +1,17 @@
+int multiply(int a, int b) {
+    int res = 0;
+    for (int i = 0; i < b; i++)
+        res += a;
+    return res;
+}
+
+int subtract(int a, int b) {
+    //...
+}
+
+
+/**
+ * Standard Solution;
+ * 
+ */
 
