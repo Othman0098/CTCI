055d0d576acc28d3ca1d18f4438b56fd89524c3a
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-04 17:44:22 -0400

    Update 17.12 BiNode.java

--- Chapter-17-Hard/17.12 BiNode.java
+++ Chapter-17-Hard/17.12 BiNode.java
@@ -1,1 +1,31 @@
 /**
+ * Tree Traversal;
+ * 
+ */
+
+public class BiNode {
+    public BiNode node1, node2;
+    public int data;
+}
+
+
+BiNode[] convertBTStoDLL(BiNode root) {
+    
+    /* res[0]: Head;    res[1]: Tail. */
+    BiNode[] res = new BiNode[2];
+    
+    if (root == null) {
+        res[0] = null;
+        res[1] = null;
+    } else {
+        BiNode[] prev = convertBTStoDLL(root.node1);
+        BiNode[] next = convertBTStoDLL(root.node2);
+    
+        root.node1 = prev[1];
+        root.node2 = next[0];
+        res[0] = prev[0];
+        res[1] = next[1];
+    }
+    
+    return res;
+}
