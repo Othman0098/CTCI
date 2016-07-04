daf43bd6e683ca90e2c8a68ab651f2e253e6e73e
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-04 17:01:03 -0400

    Update 17.11 Word Distance.java

--- Chapter-17-Hard/17.11 Word Distance.java
+++ Chapter-17-Hard/17.11 Word Distance.java
@@ -1,1 +1,43 @@
+/**
+ * Naive Thought;
+ * 
+ * Using merge sort;
+ * 
+ */
+ 
+Hashtable<String, LinkedList<Integer>> dict;
 
+void preDict(String[] text) {
+    
+    dict = new Hashtable<>();
+
+    for (int i = 0; i < text.length; i++) {
+        if (dict.containsKey(text[i])) {
+            LinkedList<Integer> tmp = dict.get(text[i]);
+            tmp.add(i);
+        } else {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            tmp.add(i);
+            dict.put(text[i], tmp);
+        }
+    }
+}
+
+int wordDistance(String word1, String word2) {
+    
+    LinkedList<Integer> list1 = dict.get(word1);
+    LinkedList<Integer> list2 = dict.get(word2);
+    
+    int res = Integer.MAX_VALUE;
+    int idx1 = 0, idx2 = 0;
+    
+    while(idx1 < list1.size() && idx2 < list2.size()) {
+        res = Math.min(Math.abs(list1.get(idx1) - list2.get(idx2)), res);
+        if (list1.get(idx1) > list2.get(idx2))
+            idx2++;
+        else
+            idx1++;
+    }
+    
+    return res;
+}
