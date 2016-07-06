fa74cdf06e1343b8d825270086d22d0755a580bf
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-06 01:26:16 -0400

    Update 17.14 Smallest K.java

--- Chapter-17-Hard/17.14 Smallest K.java
+++ Chapter-17-Hard/17.14 Smallest K.java
@@ -1,1 +1,21 @@
-
+/**
+ * Modified Binary Search;
+ * 
+ */
+ 
+/* Pseudo-code. */
+int returnSmallestK(List<> nums, int k) {
+    int pivot = nums.get(0);
+    for rest elment i of nums,
+        if (i < pivot)
+            put i in leftList;
+        else
+            put i in rightList;
+            
+    if (leftList<>.size == k - 1)
+        return pivot;
+    else if (leftList<>.size < k - 1)
+        return returnSmallestK(rightList, k - leftList<>.size - 1);
+    else
+        return returnSmallestK(leftList, k); 
+}
