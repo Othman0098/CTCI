8894ac9e1e528445d9eecfc04a8b8efd68cf2c50
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-07 15:42:15 -0400

    Update 16.18 Pattern Matching.java

--- Chapter-16-Moderate/16.18 Pattern Matching.java
+++ Chapter-16-Moderate/16.18 Pattern Matching.java
@@ -1,1 +1,11 @@
-
+/**
+ * Solution 1;
+ * 
+ * The relative order does not matter;
+ * for example pattern = aabab;
+ * 1: Find all possible three 'a' pattern;
+ * 2: Delete all 'a' pattern, thus forming a new string;
+ * 3. Decide if the new string contains two 'b' pattern;
+ * 4. Try all possible splits;
+ * 
+ */
