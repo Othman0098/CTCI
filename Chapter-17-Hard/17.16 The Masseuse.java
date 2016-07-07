9135e2d8046cab41f50597456fa46679c8e3e457
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-07 02:02:09 -0400

    Update 17.16 The Masseuse.java

--- Chapter-17-Hard/17.16 The Masseuse.java
+++ Chapter-17-Hard/17.16 The Masseuse.java
@@ -5,4 +5,17 @@
  * 
  */
  
+ /**
+  * Two array records by far the optimal set with or without item i;
+  * 
+  */
+  
+with[0] = nums[0];
+without[0] = 0;
+
+for (int i = 1; i < nums.length; i++)
+    without[i] = Math.max(with[i - 1] + without[i - 1]);
+    with[i] = nums[i] + without[i - 1];
+
+return Math.max(with[nums.length - 1], without[nums.length - 1]);
  
