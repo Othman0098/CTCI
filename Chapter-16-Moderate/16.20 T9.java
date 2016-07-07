7210476eae33bef07e4320c256a0738755bfc47f
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-07 16:56:07 -0400

    Update 16.20 T9.java

--- Chapter-16-Moderate/16.20 T9.java
+++ Chapter-16-Moderate/16.20 T9.java
@@ -1,1 +1,12 @@
+/**
+ * Brute Force;
+ * 
+ * For a given sequnce of digits; e.g. 8733
+ * Generate all possible characters sequence: e.g. tpdd, tpde, tpdf, tped ...
+ * Decide if any of them belongs to the Dict;
+ * 
+ * Very Slow;
+ * 
+ */
+ 
 
