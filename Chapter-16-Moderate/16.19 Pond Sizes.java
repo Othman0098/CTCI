e127f989311e40ea73c758343b1067e3af3678ea
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-07 16:45:33 -0400

    Update 16.19 Pond Sizes.java

--- Chapter-16-Moderate/16.19 Pond Sizes.java
+++ Chapter-16-Moderate/16.19 Pond Sizes.java
@@ -1,1 +1,32 @@
+/**
+ * Leetcode;
+ * 
+ * Depth First Search;
+ * 
+ * Need a extra boolean[][] matrix;
+ * Keep track of any pond that has been visited;
+ * 
+ */
 
+
+int[] allPonds(int[][] lands) {
+    int[] res;
+    boolean[][] isVisited = all false;
+    
+    for each land
+        if land == 0 && isVisited == false
+            res.add(dfs(thisLand));
+            
+    return res;
+} 
+
+int dfs(land) {
+    int res = 1;
+    isVisited = true;
+    
+    for all eight nearby land
+        if nextLand == 0 && isVisited == false;
+            res += dfs(nextLand);
+            
+    return res;
+}
