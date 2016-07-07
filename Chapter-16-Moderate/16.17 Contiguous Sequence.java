c4da77385488f8749547b40674206af4b27b48f6
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-07 15:32:37 -0400

    Update 16.17 Contiguous Sequence.java

--- Chapter-16-Moderate/16.17 Contiguous Sequence.java
+++ Chapter-16-Moderate/16.17 Contiguous Sequence.java
@@ -1,1 +1,18 @@
+/**
+ * Dynamic Programming;
+ * 
+ */
 
+int contSeqLargestSum(int[] nums) {
+   int max = 0;
+   int tmp = 0;
+   
+   for (int i = 0; i < nums.length; i++) {
+        tmp += nums[i];
+        max = Math.max(max, tmp);
+        if (tmp < 0)
+            tmp = 0;
+   }
+   
+   return max;
+}
