ba278a42cfa3a0929a4acbf8a9fbe12798ed25f2
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-07-07 17:02:57 -0400

    Update 16.20 T9.java

--- Chapter-16-Moderate/16.20 T9.java
+++ Chapter-16-Moderate/16.20 T9.java
@@ -8,5 +8,28 @@
  * Very Slow;
  * 
  */
+
+/**
+ * Possible optimization for brute force;
+ * 
+ * Use a trie to store the Dict;
+ * Then any invalid prefix can be terminate at early iteration;
+ * 
+ * Trie running time ? ?
+ * 
+ */
  
+ /**
+  * Optimized;
+  *
+  * Use preprocessing;
+  * Create a hash table that maps from sequence of digits to a list of Strings;
+  * Iterate the dictionary, store all digits -> words in the hash table above;
+  * 
+  * Every time read a sequence of digits, just look it up in the hash table;
+  * 
+  */
+  
+
+
 
