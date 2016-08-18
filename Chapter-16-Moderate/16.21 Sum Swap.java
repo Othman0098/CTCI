4281e3d6f87329942674bcfce5634a6ffb1e4b7a
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-18 18:11:50 -0400

    Update 16.21 Sum Swap.java

--- Chapter-16-Moderate/16.21 Sum Swap.java
+++ Chapter-16-Moderate/16.21 Sum Swap.java
@@ -1,3 +1,32 @@
 /**
+ * Straight Forward;
+ * 
+ * Find the pair which differences is half of the total sum of the two arrays;
  *
  */
+
+
+    static void sumSwap(int[] a, int[] b) {
+
+        int suma = 0, sumb = 0;
+
+        for(int i : a)
+            suma += i;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i : b) {
+            sumb += i;
+            set.add(i);
+        }
+
+        int diff = (suma - sumb) / 2;
+
+        for(int i : a)
+            if(set.contains(i - diff)) {
+                System.out.println(i + " " + (i - diff));
+                return;
+            }
+
+        return;
+    }
