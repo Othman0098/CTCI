2d03363bdc88500cb816c2ed8697cdedf15d6289
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-20 16:06:08 -0400

    Update 17.24 Max Submatrix.java

--- Chapter-17-Hard/17.24 Max Submatrix.java
+++ Chapter-17-Hard/17.24 Max Submatrix.java
@@ -3,3 +3,18 @@
  *
  */
  
+ 
+/**
+ * Little optimized;
+ * 
+ * pre-process of the matrix;
+ * auxiliary matrix keep sum from left;
+ * 
+ */
+ 
+ 
+/**
+ * O(1)
+ * To compute the sum of a submatrix;
+ * 
+ */
