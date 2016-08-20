31eaab9105cc71d779a781c20d11f81ca1aa8b06
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-08-20 14:34:07 -0400

    Update 17.19 Missing Two.java

--- Chapter-17-Hard/17.19 Missing Two.java
+++ Chapter-17-Hard/17.19 Missing Two.java
@@ -5,3 +5,21 @@
  * The diff is the missing number;
  *
  */
+ 
+ /**
+  * ?????????????
+  * 
+  * If there are TWO missing numbers,
+  * 
+  * Try Bit Manipulation;
+  * 
+  */
+  
+/**
+ * Better way to do:
+ * 
+ * Use sum of squares of the values;
+ * 
+ */
+  
+  
